a57eb04f7c55308e48d7fa5dffaf923542b8d0c1
YumairaOrozco18 <116225113+YumairaOrozco18@users.noreply.github.com>
2024-04-23 09:40:41 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,13 +3,13 @@ function obtenerUltimaOrden() {
     fetch('https://661384ea53b0d5d80f678f83.mockapi.io/comandos')
         .then(response => response.json())
         .then(data => {
-            // Ordenar los datos por fecha descendente para obtener la última orden
+            console.log('Datos obtenidos:', data); // Verificar los datos obtenidos de la API
             const ultimaOrden = data.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))[0];
-            
-            // Mostrar el texto de la última orden en la sección correspondiente del HTML
+            console.log('Última orden:', ultimaOrden); // Verificar la última orden obtenida
             const lastOrderText = document.getElementById('lastOrderText');
             lastOrderText.textContent = ultimaOrden ? ultimaOrden.orderText : 'No hay órdenes disponibles';
         })
+        
         .catch(error => {
             console.error('Error al obtener los datos:', error);
         });
