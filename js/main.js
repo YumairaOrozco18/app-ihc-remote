0b71c66ffdbd32a46faa9e1f98ef1a9c5297e872
YumairaOrozco18 <116225113+YumairaOrozco18@users.noreply.github.com>
2024-04-25 09:12:58 -0600

    Update main.js
    ya funcional

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 const lastOrderText = document.getElementById('lastOrderText');
 
-// Función para obtener el último registro de la base de datos de MockAPI
-document.addEventListener('DOMContentLoaded,', function (){
 
+
+// Función para obtener el último registro de la base de datos de MockAPI
+document.addEventListener("DOMContentLoaded", function(){
     function obtenerUltimaOrden() {
         fetch('https://661384ea53b0d5d80f678f83.mockapi.io/comandos')
             .then(response => response.json())
             .then(data => {
-                console.log('Datos obtenidos:', data); // Verificar los datos obtenidos de la API
-                const ultimaOrden = data.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))[0];
-                console.log('Última orden:', ultimaOrden); // Verificar la última orden obtenida
+                console.log('Datos obtenidos:', data);
+                const ultimaOrden = data[data.length - 1]; // Obtener el último elemento del arreglo
+                console.log('Última orden:', ultimaOrden);
                 const lastOrderText = document.getElementById('lastOrderText');
-                lastOrderText.textContent = ultimaOrden ? ultimaOrden.orderText : 'No hay órdenes disponibles';
+                lastOrderText.textContent = ultimaOrden ? ultimaOrden.orden : 'No hay órdenes disponibles';
             })
-            
             .catch(error => {
                 console.error('Error al obtener los datos:', error);
             });
